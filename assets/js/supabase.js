2d70145f2ab1432d773abacf9b96032c4fab3d65
rajeshsahu31-cyber <rajeshsahu31@gmail.com>
2026-08-30 00:48:36 +0530

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,1 +1,11 @@
+/* =====================================
+   GANIT SETU - Supabase Connection
+   ===================================== */
 
+const SUPABASE_URL = "https://cbgojvnbkosdehvwerth.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_a5XOePzNSNn72WQm_xrIAQ_cj5Z01W_";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
